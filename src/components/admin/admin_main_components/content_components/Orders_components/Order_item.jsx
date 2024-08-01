6f68c9a0c7a1ea64480b7ec92aec316cdab3d538
Your Name <rungradis15946@gmail.com>
2024-08-01 18:59:56 +0700

    Update Order_item.jsx

--- src/components/admin/admin_main_components/content_components/Orders_components/Order_item.jsx
+++ src/components/admin/admin_main_components/content_components/Orders_components/Order_item.jsx
@@ -44,26 +44,35 @@ function Order_item() {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  gap-10 ">
         {order.map((o, index) => (
           <div
             className="border-2 border-gray-300 rounded-lg p-4 shadow-lg"
             key={index}>
-            <div
-              className={`w-full flex justify-end gap-1 mb-3 lg:flex-row flex-col  ${
-                o.order_status == "2" ? "" : "hidden"
-              }`}>
-              <button
-                onClick={() => cancelOrder(o.order_id)}
-                className="btn btn-error">
-                Cancel
-              </button>
-              <button
-                onClick={() => confirmOrder(o.order_id)}
-                className="btn btn-success">
-                Confirm
-              </button>
+            <div className="w-full sm:text-center lg:flex lg:justify-center lg:items-center ">
+              <div>
+                <p className="text-center">
+                  Table number:{" "}
+                  <span className="text-4xl">{o.order_table}</span>
+                </p>
+              </div>
+              <div
+                className={`  flex justify-end gap-1 mb-3 lg:flex-row flex-col  ${
+                  o.order_status == "2" ? "" : "hidden"
+                }`}>
+                <button
+                  onClick={() => cancelOrder(o.order_id)}
+                  className="btn btn-error">
+                  Cancel
+                </button>
+                <button
+                  onClick={() => confirmOrder(o.order_id)}
+                  className="btn btn-success">
+                  Confirm
+                </button>
+              </div>
             </div>
+
             <div className="flex justify-between items-center mb-2">
               <div className="text-gray-800 font-semibold text-center">
                 Order ID: {o.order_id}

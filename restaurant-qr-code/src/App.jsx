e3648a38cb5ce5cb0c0f867d1a1cc1fca9363132
Phummiphat <poompat.jr@gmail.com>
2024-10-31 23:31:45 +0700

    ทำปุ่มหน้าแรก

--- restaurant-qr-code/src/App.jsx
+++ restaurant-qr-code/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+  const handleLoginClick = () => {navigate("/admin_login")};
   return (
     <div className="app">
       <img src="https://static.vecteezy.com/system/resources/thumbnails/008/513/899/small_2x/blue-diamond-illustration-png.png" alt="DiamondBu Logo" className="logo" />
       <h1 className="title">DiamondBu</h1>
-      <button className="login-button">เข้าสู่ระบบ</button>
+      <button className="login-button" onClick={handleLoginClick}>เข้าสู่ระบบ</button>
     </div>
   );
 }

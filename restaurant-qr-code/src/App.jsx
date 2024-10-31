daa981092b2365d9795e68fcccf2e2d759446142
Phummiphat <poompat.jr@gmail.com>
2024-10-31 20:52:38 +0700

    แก้ ui

--- restaurant-qr-code/src/App.jsx
+++ restaurant-qr-code/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div>แนะนำร้าน</div>;
+  return (
+    <div className="app">
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/008/513/899/small_2x/blue-diamond-illustration-png.png" alt="DiamondBu Logo" className="logo" />
+      <h1 className="title">DiamondBu</h1>
+      <button className="login-button">เข้าสู่ระบบ</button>
+    </div>
+  );
 }
 
 export default App;

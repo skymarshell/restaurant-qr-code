99456583cb70e4f1332b09a89a76f2714b34dc7d
skymarshell <rungradis15946@gmail.com>
2025-08-04 20:25:37 +0700

    แก้ railway deploy

--- backend/db.js
+++ backend/db.js
@@ -1,36 +1,76 @@
+// const mysql = require("mysql2");
+// require("dotenv").config();
+
+// //const db = mysql.createPool(process.env.DB_CONNECTION_STRING);
+
+// // const db = mysql.createPool({
+// //       host: "localhost",
+// //       user: "root",
+// //       password: "123456",
+// //       database: "restaurant",
+// // });
+
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   ssl: {
+//     rejectUnauthorized: true
+//   }
+// });
+
+
+// //       host: process.env.DB_HOST,
+// //       user: process.env.DB_USER,
+// //       password: process.env.DB_PASSWORD,
+// //       database: process.env.DB_DATABASE,
+// //       port: process.env.DB_PORT,
+// //       ssl:{
+// //             ca: sslCA,
+// //       }
+
+// db.getConnection((err, connection) => {
+//   if (err) {
+//     console.error("Error connecting to MySQL database:", err);
+//     return;
+//   }
+//   console.log("Connected to MySQL database");
+//   connection.release();
+// });
+
+// //200 OK:
+// //201 Created:
+// //400 Bad Request:
+// //404 Not Found:
+
+// module.exports = db;
+
+
+
+// railway app
 const mysql = require("mysql2");
 require("dotenv").config();
 
-//const db = mysql.createPool(process.env.DB_CONNECTION_STRING);
-
 const db = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "123456",
-      database: "restaurant",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // ใส่ port เช่น 27885
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
-
-//       host: process.env.DB_HOST,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_DATABASE,
-//       port: process.env.DB_PORT,
-//       ssl:{
-//             ca: sslCA,
-//       }
 
 db.getConnection((err, connection) => {
   if (err) {
-    console.error("Error connecting to MySQL database:", err);
+    console.error("❌ Error connecting to MySQL database:", err.message);
     return;
   }
-  console.log("Connected to MySQL database");
-  connection.release();
+  console.log("✅ Connected to MySQL database");
+  connection.release(); // สำคัญ: ปล่อย connection กลับเข้า pool
 });
-
-//200 OK:
-//201 Created:
-//400 Bad Request:
-//404 Not Found:
 
 module.exports = db;

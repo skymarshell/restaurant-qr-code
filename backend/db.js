59be4835d7f37054568634eaf3be1f672489421a
skymarshell <rungradis15946@gmail.com>
2025-08-10 14:58:05 +0700

    update 10/8 14.58

--- backend/db.js
+++ backend/db.js
@@ -1,84 +1,25 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-//const db = mysql.createPool(process.env.DB_CONNECTION_STRING);
-
 const db = mysql.createPool({
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT, // ใส่ port เช่น 27885
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false // ✅ แก้ตรงนี้
+    rejectUnauthorized: true
   }
-=======
-=======
->>>>>>> parent of 9945658 (แก้ railway deploy)
-=======
->>>>>>> parent of 9945658 (แก้ railway deploy)
-      host: "localhost",
-      user: "root",
-      password: "123456",
-      database: "restaurant",
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 9945658 (แก้ railway deploy)
-=======
->>>>>>> parent of 9945658 (แก้ railway deploy)
-=======
->>>>>>> parent of 9945658 (แก้ railway deploy)
 });
-
-//       host: process.env.DB_HOST,
-//       user: process.env.DB_USER,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_DATABASE,
-//       port: process.env.DB_PORT,
-//       ssl:{
-//             ca: sslCA,
-//       }
 
 db.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to MySQL database:", err);
     return;
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   console.log("✅ Connected to MySQL database");
-=======
-  console.log("Connected to MySQL database");
->>>>>>> parent of 9945658 (แก้ railway deploy)
-=======
-  console.log("Connected to MySQL database");
->>>>>>> parent of 9945658 (แก้ railway deploy)
-=======
-  console.log("Connected to MySQL database");
->>>>>>> parent of 9945658 (แก้ railway deploy)
-  connection.release();
+  connection.release(); // สำคัญ: ปล่อย connection กลับเข้า pool
 });
 
-//200 OK:
-//201 Created:
-//400 Bad Request:
-//404 Not Found:
-
 module.exports = db;
-=======
-  console.log("Connected to MySQL database");
-  connection.release();
-});
 
-//200 OK:
-//201 Created:
-//400 Bad Request:
-//404 Not Found:
-
-module.exports = db;
->>>>>>> parent of 9945658 (แก้ railway deploy)
